388f1808bf1ee7cc6cd0b77017803d4f51d7c0fa
Sali <mahcara87@gmail.com>
2020-04-14 19:55:32 +0100

    Bon modifications

--- resources/views/ventes/bon.blade.php
+++ resources/views/ventes/bon.blade.php
@@ -10,10 +10,14 @@
 }
 </style>
 
-<div class="container" id="toPrint">
+<div class="container-fluid" id="toPrint">
   <div class="row">
-    <div class="col-6">
-      <h1 style="font-size: 1.25rem;" class="mb-1 text-center"><b>Bienvenue chez Ayhem ceramique</b></h1>
+    <div class="col-4">
+      <h1 style="font-size: 1.25rem;" class="mb-1 text-center"><b>Bienvenue chez Ayham ceramique</b></h1>
+      <h1 style="font-size: 0.8rem;" class="mb-1 text-center"><b>Page facebook: Ayham ceramique</b></h1>
+      
+      <h1 style="font-size: 0.8rem;" class="mb-1 text-center"><b>Le Whatsapp w Le Viber le numéro du magasin</b></h1>
+
       <table class="table w-100" id="vente">
           <thead>
             <tr>
@@ -37,15 +41,16 @@
           </tbody>
         </table>
         <p class="mt-1 text-center"><b>Total:</b> {{ceil(ceil($total)/10)*10}}</p>
-        <p class="mt-1" style="font-size: 0.7rem;"><b>Remarque:</b> dès que le produit quitte le magasin, vous ne pouvez plus le retourner.</p>
+        <p class="mt-1" style="font-size: 0.7rem;"><b>Remarque:</b> Vérifier votre produit avant de quitter le magasin, Puiseque la maison n'accept plus le changement ou le rembourssement.</p>
+
     </div>
-    <div class="col-6">
+    <div class="col-2 border-left">
       <table class="table w-100" id="vente">
           <thead>
             <tr>
+              <th>Famille</th>
               <th>Produit</th>
-              <th>Quantité vendu</th>
-              <th>Prix total</th>
+              <th>quantite</th>
             </tr>
           </thead>
           <tbody>
@@ -53,14 +58,13 @@
               @foreach($ventes as $vente)
               @PHP( $total = $total + $vente->prix_total )
             <tr>
+              <td style="word-wrap: break-word">{{$vente->produit->familles->nom}}</td>
               <td style="word-wrap: break-word">{{$vente->produit->designation}}</td>
               <td  style="width: 30%">{{$vente->quantite}}</td>
-              <td  style="width: 30%;word-wrap: break-word">{{ceil(ceil($vente->prix_total)/10)*10}}</td>
             </tr>
               @endforeach
           </tbody>
         </table>
-        <p class="mt-3 text-center"><b>Total:</b> {{ceil(ceil($total)/10)*10}}</p>
     </div>
   </div>
 </div>
